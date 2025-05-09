1af5927b61d9caa492354e5b74e1454bf9f224a1
ILikeBread7 <pjaworski47@gmail.com>
2025-05-09 18:12:03 +0200

    Level up screen

--- ui_prototype/script.js
+++ ui_prototype/script.js
@@ -1,4 +1,10 @@
 (() => {
     console.log('script working!')
     document.getElementById('answer-input').focus();
+
+    document.getElementById('level-up-hint-close-button').addEventListener('click', () => {
+        document.getElementById('level-up-container').style.display = 'none';
+        document.getElementById('answer-input').focus();
+    });
+
 })();
