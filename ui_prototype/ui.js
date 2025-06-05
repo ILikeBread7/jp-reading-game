77a627b90b45696a6658ef220c3f4fb2ec12ff30
ILikeBread7 <pjaworski47@gmail.com>
2025-06-05 18:03:18 +0200

    More test code

--- ui_prototype/ui.js
+++ ui_prototype/ui.js
@@ -135,6 +135,13 @@ var $kt = $kt || {};
             }
 
             event.stopPropagation();
+
+            $kt.ui.showLevelExp('Level 2: か行', 123, 5, 12, [
+                { oldExpPercentage: 20, newExpPercentage: 100, addedExp: 3 },
+                { char: 'か', oldExpPercentage: 10, newExpPercentage: 100, addedExp: 1 },
+                { char: 'き', oldExpPercentage: 20, newExpPercentage: 40, addedExp: 2 }
+            ]);
+
             this.showLevelUp();
         }
 
