49043c6af9921d013ae62c2afe23a71244bb7e70
ILikeBread7 <pjaworski47@gmail.com>
2025-06-26 19:23:56 +0200

    XML parser changes

--- dict_xml_parser.js
+++ dict_xml_parser.js
@@ -7,7 +7,17 @@ fs.readFile('JMdict_e', 'utf8', (err, data) => {
         return;
     }
 
-    const parser = new XMLParser();
+    const options = {
+        ignoreAttributes: false,
+        attributeNamePrefix: '',
+        textNodeName: 'value',
+        transformAttributeName: attrName => {
+            const xmlPrefix = 'xml:';
+            return attrName.startsWith(xmlPrefix) ? attrName.slice(xmlPrefix.length) : attrName;
+        }
+    };
+
+    const parser = new XMLParser(options);
     const parsedData = parser.parse(data);
     fs.writeFile('JMdict_e.json', JSON.stringify(parsedData, null, 2), () => {
         console.log('JSON file written!');
