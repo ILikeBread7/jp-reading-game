15cec5bcdddfb4d0bd20d084ba26139644088c0c
ILikeBread7 <pjaworski47@gmail.com>
2025-09-19 16:49:41 +0200

    Fixed and added param to deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ui/dicts
-sh parsers/download_jmdict.sh
+sh parsers/download_jmdict.sh prod
--- parsers/download_jmdict.sh
+++ parsers/download_jmdict.sh
@@ -8,6 +8,6 @@ wget -P /tmp http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz
 gunzip /tmp/JMdict_e.gz
 mv /tmp/JMdict_e ./
 
-rm ui/dicts/*
+rm -f ../ui/dicts/*
 node dict_xml_parser.js
-node new_dict_json_parser.js prod
+node new_dict_json_parser.js $1
