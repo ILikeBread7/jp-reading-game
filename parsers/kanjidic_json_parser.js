279026bdb2526a8da9f6f5dc5ebef4b6d35253f0
ILikeBread7 <pjaworski47@gmail.com>
2025-10-13 19:02:32 +0200

    Kanji types added to kanji parser

--- parsers/kanjidic_json_parser.js
+++ parsers/kanjidic_json_parser.js
@@ -1,22 +1,43 @@
 import fs from 'node:fs';
 
 const kanjidic = JSON.parse(fs.readFileSync('kanjidic2.json', 'utf-8'));
+const kanjiTypes = JSON.parse(fs.readFileSync('kanji_types.json', 'utf-8'));
+const kanjiTypeMap = Object.entries(kanjiTypes).reduce((acc, [ type, kanjiString ]) => {
+    [ ...kanjiString ]
+        .forEach(kanji => !acc.has(kanji) && acc.set(kanji, type));
+    return acc;
+}, new Map());
+
+const kanjjiTypeOrder = {
+    '象形': 1,
+    '指事': 2,
+    '会意': 3,
+    '形声': 4,
+    '会意兼形声': 5,
+    '国字': 6
+};
 
 const data = kanjidic['kanjidic2']['character'].map(character => {
     const readingMeaning = character['reading_meaning'];
     const rmgroup = readingMeaning && readingMeaning['rmgroup'];
     const reading = rmgroup && rmgroup['reading'];
     const meaning = rmgroup && rmgroup['meaning'];
+    const kanji = character['literal'];
 
     return {
-        kanji: character['literal'],
+        kanji,
         grade: character['misc']['grade'],
         freq: character['misc']['freq'],
+        type: kanjiTypeMap.get(kanji),
         kunReadings: reading && reading['kunReadings'],
         onReadings: reading && reading['onReadings'],
         meaning: meaning && (elementToArray(meaning).map(meaning => meaning.toString()))
     }
-}).sort((a, b) => (numOrMax(a.grade) - numOrMax(b.grade)) || (numOrMax(a.freq) - numOrMax(b.freq)));
+}).sort((a, b) =>
+    (numOrMax(a.grade) - numOrMax(b.grade))
+    || (numOrMax(kanjjiTypeOrder[a.type]) - numOrMax(kanjjiTypeOrder[b.type]))
+    || (numOrMax(a.freq) - numOrMax(b.freq))
+);
 
 fs.writeFileSync('kanji_data.json', JSON.stringify(data, null, 2));
 console.log('kanji_data.json file written!');
