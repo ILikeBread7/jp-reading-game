dbcaf5d0ab10286337af504423f7ddd1b6be0c1f
ILikeBread7 <pjaworski47@gmail.com>
2025-10-16 19:17:38 +0200

    WIP kanji parser

--- parsers/kanjidic_json_parser.js
+++ parsers/kanjidic_json_parser.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 
 const DEBUG = process.argv[2] !== 'prod';
+const GENERATE_LEVELS = process.argv[2] === 'levels';
 const CONFIG = DEBUG
     ? { // debug config
         indentSize: 2
@@ -8,6 +9,9 @@ const CONFIG = DEBUG
     : { // prod config
         indentSize: 0
     };
+if (GENERATE_LEVELS) {
+    console.log('Running generate levels.');
+}
 console.log(DEBUG ? 'Running debug.' : 'Running prod!');
 
 const kanjidic = JSON.parse(fs.readFileSync('kanjidic2.json', 'utf-8'));
@@ -52,6 +56,13 @@ const data = kanjidic['kanjidic2']['character'].map(character => {
     || (numOrMax(a.freq) - numOrMax(b.freq))
 );
 
+const TARGET_CHARS_PER_LEVEL = 5;
+const MIN_CHARS_PER_LEVEL = TARGET_CHARS_PER_LEVEL - 1;
+if (GENERATE_LEVELS) {
+    generateLevels(data);
+    process.exit();
+}
+
 if (DEBUG) {
     fs.writeFileSync('kanji_data.json', JSON.stringify(data, null, 2));
     console.log('kanji_data.json file written!');
@@ -88,4 +99,57 @@ function elementToArray(element) {
 
 function numOrMax(number) {
     return number ?? Number.MAX_SAFE_INTEGER;
+}
+
+function generateLevels(data) {
+    const levels = [];
+    let currentLevel = [];
+    let lastGrade = 1;
+
+    const firstOfType = new Set();
+
+    for (const entry of data) {
+        const kanji = entry.kanji;
+
+        // Grades 9 and 10 are both Jinmeiyo
+        if (entry.grade !== 10 && entry.grade !== lastGrade) {
+            lastGrade = entry.grade;
+            if (currentLevel.length > 0) {
+                addLevel(levels, currentLevel);
+                currentLevel = [];
+            }
+        }
+
+        currentLevel.push(kanji);
+        const type = entry.type;
+        if (type && !type.startsWith('漢字検定') && !firstOfType.has(type)) {
+            firstOfType.add(type);
+            currentLevel[currentLevel.length - 1] += `|${type}`;
+        }
+
+        if (currentLevel.length === TARGET_CHARS_PER_LEVEL) {
+            addLevel(levels, currentLevel);
+            currentLevel = [];
+        }
+    }
+
+    if (currentLevel.length > 0) {
+        addLevel(levels, currentLevel);
+    }
+
+    fs.writeFileSync('levels.txt', JSON.stringify(levels, null, 2));
+    console.log('Levels file written!');
+}
+
+function addLevel(levels, level) {
+    levels.push(level);
+
+    for (let i = levels.length - 1; i > 0 && levels[i].length < MIN_CHARS_PER_LEVEL; i--) {
+        const currentLevel = levels[i];
+        const previousLevel = levels[i - 1];
+        const missingCharsNumber = MIN_CHARS_PER_LEVEL - currentLevel.length;
+
+        const charsToMove = previousLevel.splice(previousLevel.length - missingCharsNumber, missingCharsNumber);
+        currentLevel.unshift(...charsToMove);
+    }
 }
