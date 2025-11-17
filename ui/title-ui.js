383e92577fbd3ca4c4b6dfd395a5ca554fc90f5a
ILikeBread7 <pjaworski47@gmail.com>
2025-11-17 19:26:31 +0100

    Arcade button menu fix

--- ui/title-ui.js
+++ ui/title-ui.js
@@ -41,7 +41,7 @@ class KantoreTitleUi {
     }
 
     startTitleScene() {
-        if (this._isPreTitleVisible()) {
+        if (this._isPreTitleVisible() || dialogue.isVisible()) {
             return;
         }
 
@@ -245,6 +245,14 @@ class KantoreTitleUi {
 
     _startArcadeMode(categoryName, dict) {
         dict.preload();
+
+        // Prevent underlying menu button
+        // from being "clicked" on enter
+        const focusedItem = document.activeElement;
+        if (focusedItem) {
+            focusedItem.blur();
+        }
+
         dialogue.show(
             'Arcade mode',
             KantoreTemplates.arcadeModeExplanation(),
